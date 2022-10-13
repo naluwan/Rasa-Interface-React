0e6725888fdb31771d2c3c59052cb2552da7031d
naluwan <street.baller06@gmail.com>
2022-10-13 16:13:22 +0800

    feat: update react & react-dom version

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-// import ReactDOM from 'react-dom/client';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 
-const root = document.getElementById('root');
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <HashRouter>
     <App />
   </HashRouter>,
-  root,
 );
