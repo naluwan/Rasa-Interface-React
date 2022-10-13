51536d732df0b2c5050c8ba622413d145257bfbf
naluwan <street.baller06@gmail.com>
2022-10-13 14:43:36 +0800

    feat: modify navitems index

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
 export const NAVITEMS = [
-  { id: '1', name: '對話紀錄', link: '/conversion' },
-  { id: '2', name: '故事流程', link: '/stories' },
+  { id: '1', name: '故事流程', link: '/stories' },
+  { id: '2', name: '對話紀錄', link: '/conversion' },
 ];
